818b8eaa032a964374ead4be61031f120a2c1e12
MiltonXXL <fredrikmilton39@gmail.com>
2024-11-25 19:36:49 +0100

    Add project files.

---
+++ ADO.net2/MovieStore.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.net2
+{
+    public class MovieStore
+    {
+        public SqlCommand GetActorFilms(SqlConnection connection)
+        {
+            Console.WriteLine("Välj skådespelares förnamn: ");
+            string? actorFirstName = Console.ReadLine();
+            Console.WriteLine("Välj skådespelares efternamn: ");
+            string? actorLastName = Console.ReadLine();
+
+            SqlCommand command = new SqlCommand($"select film.title " +
+                $"                                       from actor " +
+                $"                                       inner join film_actor on actor.actor_id = film_actor.actor_id" +
+                $"                                       inner join film on film_actor.film_id = film.film_id" +
+                $"                                       where first_name = '{actorFirstName}' " +
+                $"                                       and last_name = '{actorLastName}'" +
+                $"                                       ", connection);
+            return command;
+        }
+    }
+}
---
+++ ADO.net2/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlTypes;
+
+namespace ADO.net2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MovieStore store = new MovieStore();
+
+            SqlConnection? connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Sakila;" +
+                                                                        "Integrated Security=True;Connect Timeout=30;Encrypt=False;" +
+                                                                        "Trust Server Certificate=False;Application Intent=ReadWrite;" +
+                                                                        "Multi Subnet Failover=False");
+            SqlCommand command = store.GetActorFilms(connection);
+
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            Console.WriteLine("Aktuella filmer: ");
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0]);
+            }
+            connection.Close();
+        }
+    }
+}
